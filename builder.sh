2dcdb80ce01267a312aeb6d13b7da322026fd181
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-04-28 20:53:45 +0200

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,11 +3,11 @@
 APP=whatsapp-nativefier
 mkdir tmp
 cd ./tmp
-wget -q $(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | grep -v zsync | grep -i continuous | grep -i appimagetool | grep -i x86_64 | grep browser_download_url | cut -d '"' -f 4 | head -1) -O appimagetool
+wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
 chmod a+x ./appimagetool
 
-VERSION=$(wget -q https://api.github.com/repos/frealgagu/archlinux.whatsapp-nativefier/releases/latest -O - | grep -E tag_name | awk -F '[""]' '{print $4}')
-URL=$(wget -q https://api.github.com/repos/frealgagu/archlinux.whatsapp-nativefier/releases -O - | grep -w -v i386 | grep -w -v i686 | grep -w -v aarch64 | grep -w -v arm64 | grep -w -v armv7l | grep browser_download_url | grep -w -v debug  | grep -i "x86_64.pkg.tar.zst" | cut -d '"' -f 4 | head -1)
+VERSION=$(curl -Ls https://api.github.com/repos/frealgagu/archlinux.whatsapp-nativefier/releases/latest | grep -E tag_name | awk -F '[""]' '{print $4}')
+URL=$(curl -Ls https://api.github.com/repos/frealgagu/archlinux.whatsapp-nativefier/releases | grep -w -v i386 | grep -w -v i686 | grep -w -v aarch64 | grep -w -v arm64 | grep -w -v armv7l | grep browser_download_url | grep -w -v debug  | grep -i "x86_64.pkg.tar.zst" | cut -d '"' -f 4 | head -1)
 wget $URL
 tar xf ./*.tar.zst
 mkdir $APP.AppDir
