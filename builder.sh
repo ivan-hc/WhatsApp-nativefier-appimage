3cb19da7f41944239afa3521248545c253c1452b
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-19 03:00:08 +0200

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 APP=whatsapp-nativefier
 mkdir tmp
 cd ./tmp
-wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
+wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 chmod a+x ./appimagetool
 
 VERSION=$(curl -Ls https://api.github.com/repos/frealgagu/archlinux.whatsapp-nativefier/releases/latest | grep -E tag_name | awk -F '[""]' '{print $4}')
@@ -22,6 +22,6 @@ export UNION_PRELOAD="${HERE}"
 exec "${HERE}"/WhatsApp "$@"
 EOF
 chmod a+x ./$APP.AppDir/AppRun
-ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP.AppDir
+ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP.AppDir
 cd ..
 mv ./tmp/*.AppImage ./Whatsapp-nativefier-$VERSION-x86_64.AppImage
